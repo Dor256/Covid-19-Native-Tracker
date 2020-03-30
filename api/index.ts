f29113b6bae8644c9f457bdc3ce6544b0e1f8a33
Dor <dorjp6@gmail.com>
2020-03-30 16:23:14 +0300

    Change population api to use v1 - aligns better with covid19 api

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import { WORLD_POP, SOUTH_KOREA, SOUTH_KOREA_POP_SEARCH } from '../consts';
+import { WORLD_POP } from '../consts';
 import { States } from '../us-states';
 
 export type CountryInfo = {
@@ -69,7 +69,7 @@ export interface PopulationApi {
 }
 
 const COVID_URL = 'https://corona.lmao.ninja';
-const POPULATION_URL = 'https://restcountries.eu/rest/v2';
+const POPULATION_URL = 'https://restcountries.eu/rest/v1';
 
 export const coronaApi: CoronaApi = {
   getAllCases() {
@@ -102,8 +102,7 @@ export const coronaApi: CoronaApi = {
 
 const populationApi: PopulationApi = {
   getPopulationByCountry(country: string) {
-    const countryToSearch = country === SOUTH_KOREA ? SOUTH_KOREA_POP_SEARCH : country;
-    return fetch(`${POPULATION_URL}/name/${countryToSearch}?fullText=true&fields=population`)
+    return fetch(`${POPULATION_URL}/name/${country}?fullText=true&fields=population`)
       .then((response) => response.json()
         .then((data) => data[0].population));
   }
--- consts/index.ts
+++ consts/index.ts
@@ -1,4 +1,2 @@
 export const WHOLE = 100;
 export const WORLD_POP = 7800000000;
-export const SOUTH_KOREA = 'south korea';
-export const SOUTH_KOREA_POP_SEARCH = 'Korea (Republic of)';
